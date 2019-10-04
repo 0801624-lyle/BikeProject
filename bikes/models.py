518574e00a811a914139f2fe8d47f69b59cd1e9a
Alexander <alexanderberg94@gmail.com>
2019-10-04 10:55:27 +0100

    Created tables

--- bikes/models.py
+++ bikes/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bikes(models.Model):
+    status = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 200)
+    condition = models.CharField(max_length = 200)
+    user = models.ForeignKey('User_profile', on_delete = models.SET_NULL)
+
+class User_profile(models.Model):
+    #user_type = models.CharField(max_length = 200)
+    #username = models.CharField(max_length = 200)
+    #email = models.CharField(max_length = 200)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    balance = models.FloatField(default=0)
+    charges = models.FloatField(default=0)
+
+class Location(models.Model):
+    station_name = models.CharField(max_length = 200)
+    num_bikes = models.IntegerField(default=0)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+class User_history(models.Model):
+    user_id = models.ForeignKey(User_profile)
+    bike_id = models.ForeignKey(Bikes)
+    date_hired = models.DateTimeField()
+    date_returned = models.DateTimeField()
+    num_hires = models.IntegerField()
+    start_station = models.ForeignKey(Location)
+    end_station = models.ForeignKey(Location)
