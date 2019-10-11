401c52be7f3b9766a98e63110dfc7aa593370bb8
0801624-lyle <0801624s@student.gla.ac.uk>
2019-10-11 16:56:20 +0100

    added dynamic username/email lookup for registration form

--- bikes/urls.py
+++ bikes/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name="bikes/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('register', views.RegistrationView.as_view(), name='register'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
+
+    # ajax views
+    path('register/ajax/check_username/', views.validate_username, name='ajax_check_username'),
+    path('register/ajax/check_email/', views.validate_email, name='ajax_check_email')
 ]
--- bikes/views.py
+++ bikes/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 
@@ -9,6 +11,10 @@ from .forms import RegistrationForm
 # Create your views here.
 def index(request):
     return render(request, 'bikes/index.html', {})
+
+def profile(request):
+    return HttpResponse("profile") 
+
 
 
 class RegistrationView(SuccessMessageMixin, CreateView):
@@ -26,5 +32,27 @@ class RegistrationView(SuccessMessageMixin, CreateView):
         login(self.request, user)
         return valid
 
-def profile(request):
-    return render(request, 'bikes/profile.html')
+
+# Ajax views
+def validate_username(request):
+    """ Called when a user is registering and typing their username.
+        Every key typed will send a request to this view function, to check whether the username is taken.
+        If the username matches a username in the database, the user should be prevented from signing up
+    """
+    username = request.GET.get('username', None)
+    username_exists = User.objects.filter(username__iexact=username).exists()
+    return JsonResponse({
+        "username_exists": username_exists
+    })
+
+def validate_email(request):
+    """ Called when a user is registering and typing their email address.
+        Every key typed will send a request to this view function, to check whether the email is taken.
+        If the email matches an email address in the database, the user should be prevented from signing up
+        unless they change the email address
+    """
+    email = request.GET.get('email', None)
+    email_exists = User.objects.filter(email__iexact=email).exists()
+    return JsonResponse({
+        "email_exists": email_exists
+    })
