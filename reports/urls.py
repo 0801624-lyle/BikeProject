5ab4f1da4726dd5c68dd7e4d5f29a888bb3ae9da
0801624-lyle <0801624s@student.gla.ac.uk>
2019-11-02 13:35:47 +0000

    bunch of reporting functions added

--- reports/urls.py
+++ reports/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.reports_index, name='reports_index'),
     path('bike-locations/', views.bike_locations, name='bike_locations'),
     path('user-report/', views.user_report, name='user-report'), 
+    path('financial-report/', views.financial_report, name='financial-report'),
 ]
